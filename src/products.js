5f1eccd1caf76129533496f6bc22b784f732e285
suman-samanta <sumansamanta110@gmail.com>
2023-09-13 04:10:29 +0530

    unbundletask1

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/7.jpg";
+import product2 from "./assets/products/8.jpg";
+import product3 from "./assets/products/1.jpg";
+import product4 from "./assets/products/2.jpg";
+import product5 from "./assets/products/3.webp";
+import product6 from "./assets/products/4.jpeg";
+import product7 from "./assets/products/5.jpg";
+import product8 from "./assets/products/6.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Marce Chocolates",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Highburry Classic",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Cadberry Celebrations",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Furr Choco",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Raymonds",
+    price: 199,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Raymond classic",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Wang Chocos",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Nutties",
+    price: 40.0,
+    productImage: product8,
+  },
+];
